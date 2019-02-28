e505dca73e85900db77acf5d6c704fe259749503
mpavone <Manuel.PAVONE@amadeus.com>
2019-02-28 19:39:38 +0100

    implem

--- src/com/sec/models/Slide.java
+++ src/com/sec/models/Slide.java
@@ -1,17 +1,27 @@
 package com.sec.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Slide {
   int id;
   boolean vertical;
   Photo photo1;
   Photo photo2;
+  Set<String> tags;
 
   public Slide(Photo photo1) {
     this.photo1 = photo1;
+    this.tags = new HashSet<>();
   }
 
   public Slide(Photo photo1, Photo photo2) {
     this.photo1 = photo1;
     this.photo2 = photo2;
   }
+
+  public double nbOfPointsMax() {
+    return Math.floor(tags.size()/ 2.);
+  }
+
 }
